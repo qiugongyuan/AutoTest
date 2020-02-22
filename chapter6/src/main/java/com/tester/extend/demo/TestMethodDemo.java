19666ab160380b57e47f76466761f2c6582d5edf
qiugongyuan <360960443@qq.com>
2020-02-22 17:01:14 +0800

    增加chapter7

--- chapter6/src/main/java/com/tester/extend/demo/TestMethodDemo.java
+++ chapter6/src/main/java/com/tester/extend/demo/TestMethodDemo.java
@@ -1,4 +1,37 @@
 package com.tester.extend.demo;
 
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import org.testng.internal.InvokeMethodRunnable;
+
 public class TestMethodDemo {
+    @Test
+
+    public void test1(){
+        Assert.assertEquals(3,2);
+
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,1);
+
+    }
+
+
+    @Test
+    public void test3(){
+        Assert.assertEquals(1,1);
+    }
+
+
+    @Test
+    public void LogerTest() {
+        Reporter.log("这是自己的");
+
+    }
+
+
+
 }
